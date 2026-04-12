3bce7ee2c116e16dba4e5e9c9cdc665ab2d65019
AronC500 <aronchen500@gmail.com>
2026-04-12 01:09:08 -0400

    testing

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include "board.h"
 
-#include <iostream>
-#include "board.h"
-
 int main() {
     Board board;
-    board.print();
-
-   
+    while (true) {
+        Move whiteMove = board.getBestMove(5, true); 
+        board.makeMove(whiteMove);
+        board.print();
+        std::cout << std::endl;
+        if (board.isGameOver(false)) {
+            std::cout << "Game Over. Black Lost." << std::endl;
+            break;
+        }
+        
+        Move blackMove = board.getBestMove(1, false); 
+        board.makeMove(blackMove);
+        board.print();
+        std::cout << std::endl;
+        if (board.isGameOver(true)) {
+            std::cout << "Game Over. White Lost." << std::endl;
+            break;
+        }
+        if (board.isDraw(true) || board.isDraw(false)) {
+            std::cout << "Draw." << std::endl;
+           
+            break;
+        }
+    }
 
     return 0;
 }
